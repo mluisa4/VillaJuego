fe50da8de9a731f85127d05bb144359b30e1d882
mluisa4 <35712025+mluisa4@users.noreply.github.com>
2020-07-04 19:07:43 -0400

    Carga de imágenes en canvas
    Cargar imágenes en Canvas en un sitio especifico.

--- platzi.js
+++ platzi.js
@@ -1,11 +1,50 @@
-var z;
+var villaM = document.getElementById("villaMaria");
+var papel = villaM.getContext("2d");
+var mapa = "tile.png";
+
+var fondo = new Image(); // creo objeto Image en imagen
+fondo.src = mapa; //cargo la imagen tile-mapa en el objeto imagen con el metodo src
+fondo.addEventListener("load", dibujar);//escucho el evento de cargar la imagen con "load"
+
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load", dibujarVacas);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load", dibujarCerdos);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load", dibujarPollos);
+
+function dibujar()
+{
+  papel.drawImage(fondo, 0, 0);
+}
+
+
+function dibujarVacas()
+{
+  papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdos()
+{
+  papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollos()
+{
+  papel.drawImage(pollo, 300, 150);
+}
 
 //Funcion que mustra 10 numeros de forma aleatoria entr el rango especificado
-for(var i=0; i<10; i++)
-{
-    var z = aleatorio(10, 20);
-    document.write(z + ",");
-}
+//for(var i=0; i<10; i++)
+//{
+  //  var z = aleatorio(10, 20);
+    //document.write(z + ",");
+//}
 
 function aleatorio(min, max)
 {
