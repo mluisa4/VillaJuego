a48cc331fb4439ad50b7f2ca72fb6d28c25ae5c6
mluisa4 <35712025+mluisa4@users.noreply.github.com>
2020-07-04 19:36:50 -0400

    Optimización de Carga de imágenes
    Optimización de la carga de imágenes añadiendo Jason. Carga de imagen de vaca primeramente, aun de manera especifica dentro del canvas.

--- platzi.js
+++ platzi.js
@@ -1,43 +1,76 @@
 var villaM = document.getElementById("villaMaria");
 var papel = villaM.getContext("2d");
-var mapa = "tile.png";
+//var mapa = "tile.png";
 
-var fondo = new Image(); // creo objeto Image en imagen
-fondo.src = mapa; //cargo la imagen tile-mapa en el objeto imagen con el metodo src
-fondo.addEventListener("load", dibujar);//escucho el evento de cargar la imagen con "load"
+var fondo = {
+  url: "tile.png",
+  cargaOk: false
+}
+var vaca = {
+  url: "vaca.png",
+  cargaOk: false
+}
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load", dibujarVacas);
+fondo.imagen = new Image(); // creo objeto Image en imagen
+fondo.imagen.src = fondo.url; //cargo la imagen tile-mapa en el objeto imagen con el metodo src
+fondo.imagen.addEventListener("load", cargarFondo);//escucho el evento de cargar la imagen con "load"
 
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load", dibujarCerdos);
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
 
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load", dibujarPollos);
+function cargarFondo()
+{
+  fondo.cargaOk = true;
+  dibujar();
+}
+
+function cargarVacas()
+{
+  vaca.cargaOk = true;
+  dibujar();
+}
 
 function dibujar()
 {
-  papel.drawImage(fondo, 0, 0);
+    if(fondo.cargaOk == true)
+      {
+          papel.drawImage(fondo.imagen, 0,0);
+      }
+    if(vaca.cargaOk == true)
+      {
+          papel.drawImage(vaca.imagen, 100,100);
+      }
 }
 
+//var cerdo = new Image();
+//cerdo.src = "cerdo.png";
+//cerdo.addEventListener("load", cargarCerdos);
 
-function dibujarVacas()
-{
-  papel.drawImage(vaca, 10, 10);
-}
+//var pollo = new Image();
+//pollo.src = "pollo.png";
+//pollo.addEventListener("load", cargarPollos);
 
-function dibujarCerdos()
-{
-  papel.drawImage(cerdo, 10, 300);
-}
+//function dibujar()
+//{
+//  papel.drawImage(fondo, 0, 0);
+//}
 
-function dibujarPollos()
-{
-  papel.drawImage(pollo, 300, 150);
-}
+
+//function dibujarVacas()
+//{
+//  papel.drawImage(vaca, 10, 10);
+//}
+
+//function dibujarCerdos()
+//{
+//  papel.drawImage(cerdo, 10, 300);
+//}
+
+//function dibujarPollos()
+//{
+//  papel.drawImage(pollo, 300, 150);
+//}
 
 //Funcion que mustra 10 numeros de forma aleatoria entr el rango especificado
 //for(var i=0; i<10; i++)
